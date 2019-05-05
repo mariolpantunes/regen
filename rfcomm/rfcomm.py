0a5278762c848df6817da4cd111c89f8240b004d
Mário Antunes <mariolpantubes@gmail.com>
2019-05-05 22:44:58 +0100

    Added simple rfcomm bridge...

---
+++ rfcomm/rfcomm.py
@@ -0,0 +1,75 @@
+# coding: utf-8
+
+
+__author__ = "Mário Antunes"
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Mário Antunes"
+__email__ = "mariolpantunes@gmail.com"
+__status__ = "Development"
+
+
+import json
+import socket
+import signal
+import logging
+import argparse
+import datetime
+from influxdb import InfluxDBClient
+
+
+logger = logging.getLogger('RFComm')
+logger.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%m-%d %H:%M:%S')
+handler = logging.RotatingFileHandler('rfcomm.log', maxBytes=20, backupCount=2)
+logger.addHandler(handler)
+
+
+global done = False
+
+
+def exit_gracefully():
+    global done = True
+
+
+def main(args):
+    signal.signal(signal.SIGINT, exit_gracefully)
+    signal.signal(signal.SIGTERM, exit_gracefully)
+
+    while global not done:
+        try:
+            client = InfluxDBClient('localhost', 8086, '', '', 'regen')
+            s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+            s.connect((args.addr, 1))
+
+            while global not done:
+                packet = ''
+                while not packet or packet[-1] != '}': 
+                    data = s.recv(256)
+                    packet += data.decode('utf-8')
+                jdata = json.loads(packet)
+                logging.debug('JSON %s', jdata)
+                json_body = [{'measurement':'voltage',
+                    'tags':{'address':args.addr},
+                    'time':datetime.datetime.now().iso_format(),
+                    'fields':{'value': jdata['voltage']}},
+                    {'measurement':'ampere',
+                    'tags':{'address':args.addr},
+                    'time':datetime.datetime.now().iso_format(),
+                    'fields':{'value': jdata['ampere']}},
+                    {'measurement':'watt',
+                    'tags':{'address':args.addr},
+                    'time':datetime.datetime.now().iso_format(),
+                    'fields':{'value': jdata['watt']}}]
+        except Exception as e:
+            logger.error('%s', e)
+                
+    s.close()
+    client.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='RFComm - Reads data from the bt')
+    parser.add_argument('--addr', type=str, help='target address', default='FC:A8:9A:00:52:7E')
+    args = parser.parse_args()
+    main(args)
+
