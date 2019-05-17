3fcce498afb7d7319c7d9c4cd72c4c2e2f0371ce
Mário Antunes <mariolpantubes@gmail.com>
2019-05-17 17:26:07 +0100

    Added a version of the rfcomm that support 3 power sensors.

--- rfcomm/rfcomm.py
+++ rfcomm/rfcomm.py
@@ -16,13 +16,13 @@ import signal
 import logging
 import argparse
 import datetime
+from logging import handlers
 from influxdb import InfluxDBClient
-from logging.handlers import RotatingFileHandler
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%m-%d %H:%M:%S')
 logger = logging.getLogger('RFComm')
-handler = logging.handlers.SysLogHandler(address = '/dev/log')
+handler = handlers.SysLogHandler(address = '/dev/log')
 logger.addHandler(handler)
 
 
@@ -31,8 +31,9 @@ done = False
 
 def exit_gracefully(signum, frame):
     global done
-    done = True
-    logger.info('Exit gracefully...')
+    if not done:
+        done = True
+        logger.info('Exit gracefully...')
 
 
 def main(args):
@@ -43,52 +44,80 @@ def main(args):
 
     while not done:
         try:
-            client = InfluxDBClient('localhost', 8086, '', '', 'regen')
-            s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+            #client = InfluxDBClient('localhost', 8086, '', '', 'regen')
+            s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)  
+            s.connect((args.addr, args.port))
             s.settimeout(args.timeout)
-            s.connect((args.addr, 1))
-
+            
             while not done:
                 packet = ''
                 while not packet or packet[-1] != '}': 
-                    data = s.recv(256)
+                    data = s.recv(128)
                     packet += data.decode('utf-8')
                 jdata = json.loads(packet)
                 logger.debug('JSON %s', jdata)
 
                 current_time = datetime.datetime.utcnow().isoformat()
+
+                a0 = jdata['a0']
+                a1 = jdata['a1']
+                a2 = jdata['a2']
+
                 json_body = [{'measurement':'voltage',
-                    'tags':{'address':args.addr},
+                    'tags':{'address':args.addr, 'id':'a0'},
                     'time':current_time,
-                    'fields':{'value': float(jdata['voltage'])}},
+                    'fields':{'value': float(a0['voltage'])}},
+                    {'measurement':'voltage',
+                    'tags':{'address':args.addr, 'id':'a1'},
+                    'time':current_time,
+                    'fields':{'value': float(a1['voltage'])}},
+                    {'measurement':'voltage',
+                    'tags':{'address':args.addr, 'id':'a2'},
+                    'time':current_time,
+                    'fields':{'value': float(a2['voltage'])}},
+
                     {'measurement':'ampere',
-                    'tags':{'address':args.addr},
+                    'tags':{'address':args.addr, 'id':'a0'},
                     'time':current_time,
-                    'fields':{'value': float(jdata['ampere'])}},
+                    'fields':{'value': float(a0['ampere'])}},
+                    {'measurement':'ampere',
+                    'tags':{'address':args.addr, 'id':'a1'},
+                    'time':current_time,
+                    'fields':{'value': float(a1['ampere'])}},
+                    {'measurement':'ampere',
+                    'tags':{'address':args.addr, 'id':'a2'},
+                    'time':current_time,
+                    'fields':{'value': float(a2['ampere'])}},
+
                     {'measurement':'watt',
-                    'tags':{'address':args.addr},
+                    'tags':{'address':args.addr, 'id':'a0'},
                     'time':current_time,
-                    'fields':{'value': float(jdata['watt'])}}]
+                    'fields':{'value': float(a0['watt'])}},
+                    {'measurement':'watt',
+                    'tags':{'address':args.addr, 'id':'a1'},
+                    'time':current_time,
+                    'fields':{'value': float(a1['watt'])}},
+                    {'measurement':'watt',
+                    'tags':{'address':args.addr, 'id':'a2'},
+                    'time':current_time,
+                    'fields':{'value': float(a2['watt'])}}]
                 logger.debug('JSON BODY %s', json_body)
-                client.write_points(json_body, time_precision='ms')
-                time.sleep(5)
+                #client.write_points(json_body, time_precision='ms')
         except Exception as e:
             logger.error('%s', e)
-            time.sleep(args.sleep)
+            if not done:
+                time.sleep(args.sleep)
         finally:
             s.close()
-            client.close()
-                
-    
-    s.close()
-    client.close()
+            #client.close()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RFComm - Reads data from the bt')
     parser.add_argument('--addr', type=str, help='target address', default='FC:A8:9A:00:52:7E')
-    parser.add_argument('--sleep', type=int, help='sleep when device is not transmitting', default=10)
-    parser.add_argument('--timeout', type=int, help='timeout for the BT socket', default=1)
+    parser.add_argument('--port', type=int, help='target port', default=1)
+    parser.add_argument('--sleep', type=int, help='sleep when device is not transmitting', default=5)
+    parser.add_argument('--timeout', type=int, help='timeout for the BT socket', default=10)
     args = parser.parse_args()
     main(args)
 
